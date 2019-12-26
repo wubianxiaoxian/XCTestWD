131025d937dc379a34daaa8db6c0e1293c8061f0
孙凯峰 <sunkaifeng@sunkaifengdeMacBook-Pro.local>
2019-12-26 11:26:49 +0800

    set bundle id

---
+++ test/xctest.test.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const assert = require('assert');
+
+const XCTest = require('..');
+
+describe('test', function() {
+  it('should be ok', function() {
+    assert.ok(XCTest);
+  });
+});
